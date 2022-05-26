ba14e37e61a1e618ff492c42f13546df8bc12993
Andrey Sakharov <sakharov.a@n-t.io>
2022-05-26 17:09:03 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { config } from './config';
+import path = require('path');
+
+const { name, version, description } = require(path.join(process.cwd(), 'package.json'));
 
 (async () => {
    const app = await NestFactory.create<NestFastifyApplication>(
       AppModule,
       new FastifyAdapter(),
    );
+
+   const swaggerConfig = new DocumentBuilder()
+      .setTitle(name)
+      .setDescription(description)
+      .setVersion(version)
+      .build();
+   const document = SwaggerModule.createDocument(app, swaggerConfig);
+   SwaggerModule.setup('api', app, document);
+
    await app.listen(config.APP_PORT);
 })();
